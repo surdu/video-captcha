95577230190ebb47c8ee706a007d087788cd360f
rmaria <maria_robert_daniel@yahoo.com>
2020-08-31 13:38:05 +0300

    minimize video window

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,10 @@
 const startBtn = document.getElementById("startBtn");
 const preview = document.getElementById("preview");
-const video = document.querySelector("#preview video");
+const video = document.getElementById("previewVideo");
 const numbers = document.getElementById("numbers");
 const html = document.querySelector("body");
 const thanks = document.getElementById("thanks");
+const overlay = document.getElementsByClassName('overlay')[0]
 
 const videoConstraints = {
 	audio: true,
@@ -15,8 +16,8 @@ const videoConstraints = {
 };
 
 const sequences = [
-	"black:1000|002B7F:1000|FCD116:1000|CE1126:1000|lime:1000|fff:0",
-	"red:1000|lime:1000|black:1000|magenta:1000|purple:1000|white:0"
+	"black:1000|002B7F:1000|FCD116:1000|CE1126:1000|lime:1000|transparent:0",
+	"red:1000|lime:1000|black:1000|magenta:1000|purple:1000|transparent:0"
 ];
 const sequenceIndex = random(sequences.length);
 
@@ -38,20 +39,62 @@ function flashSequence(sequence) {
 
 	function renderItem() {
 		const [item] = items.splice(0, 1);
-
+		//const freq = 14500;
+		//const dur = 400;
+        //playBeepBeep(freq, dur);
 		if (item) {
-			html.style.backgroundColor = item.color;
+    		const overlay = document.createElement('div')
+    		overlay.style.backgroundColor = item.color
+			overlay.style.width = "100vw"
+			overlay.style.height = "100vh"
+			overlay.style.position ="absolute"
+			overlay.style.top = "0"
+			overlay.style.left = "0"
+			
+			const body = document.getElementsByTagName('body')[0]
+			body.appendChild(overlay)
+			//html.style.backgroundColor = item.color;
+			//playBeep();
 			setTimeout(renderItem, item.duration);
 		}
 	}
+	console.log('should disappear')
 }
+
+
+const cycleColors = (durations, startTime, classNames) => {
+    setTimeout(durations.forEach((duration, idx) =>
+        setTimeout(()=>{
+        console.log(classNames[idx])
+        overlay.classList = "overlay " + classNames[idx] 
+    }, duration)), startTime)
+}
+
+function playBeepBeep(freq, dur) {
+	const oscilator = audioContext.createOscillator()
+	const gain = audioContext.createGain()
+	oscilator.connect(gain);
+	var frequency = freq;
+	oscilator.frequency.value = frequency;
+	oscilator.type = "sine"
+	//oscillator.frequency.value = frequency;
+	oscilator.connect(audioContext.destination)
+	oscilator.start()
+
+	setTimeout(function() {
+		oscilator.stop();
+	}, dur);
+}
+
 
 function playBeep() {
 	const oscilator = audioContext.createOscillator()
 	const gain = audioContext.createGain()
 	oscilator.connect(gain);
-
+	var frequency = 2500;
+	oscilator.frequency.value = frequency;
 	oscilator.type = "sine"
+	//oscillator.frequency.value = frequency;
 	oscilator.connect(audioContext.destination)
 	oscilator.start()
 
@@ -59,6 +102,10 @@ function playBeep() {
 		oscilator.stop();
 	}, 100);
 }
+
+
+
+
 
 function faitAttention() {
 	return new Promise(function(resolve) {
@@ -79,6 +126,7 @@ function faitAttention() {
 		}, 1000);
 	})
 }
+
 
 preview.style.display = "none";
 
@@ -129,13 +177,53 @@ startBtn.addEventListener("click", function() {
 				console.log(mediaRecorder.state);
 
 				setTimeout(function(){
-					flashSequence(sequences[sequenceIndex]);
-					playBeep();
-
+    				//var freq = 16000;
+            		//var dur = 2000;
+                    //playBeepBeep(freq, dur);
+                    var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+                    function playNote(frequency, duration) {
+                      // create Oscillator node
+                      var oscillator = audioCtx.createOscillator();
+                    
+                      oscillator.type = 'square';
+                      oscillator.frequency.value = frequency; // value in hertz
+                      oscillator.connect(audioCtx.destination);
+                      oscillator.start();
+                    
+                      setTimeout(
+                        function() {
+                          oscillator.stop();
+                          playMelody();
+                        }, duration);
+                    }
+                    
+                    function playMelody() {
+                      if (notes.length > 0) {
+                        note = notes.pop();
+                        playNote(note[0], note[1]);
+                      }
+                    }
+                    
+                    
+                    notes = [
+                      [12000, 500],
+                      [13000, 1000],
+                      [14000, 500],
+                      [15000, 1000],
+                      [16000, 500]
+                    ];
+                    
+                    notes.reverse();
+                    tempo = 100;
+                    playMelody();
+                    
+					//flashSequence(sequences[sequenceIndex]);
+					//playBeep();
+                    cycleColors([1100,2000, 2050, 5000, 6000, 6030], 500, ['white', 'black', 'transparent','white', 'black', 'transparent'])
 					setTimeout(function() {
 						mediaRecorder.stop();
 						video.pause();
-					}, 5000);
+					}, 10000);
 				}, 1000);
 			});
 		})
